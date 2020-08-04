8d051e18d79933889cb8993dfebd83c78465ff33
martin castellano <martin.castellano89@gmail.com>
2020-08-04 13:10:43 -0300

    porfolio web initial commit

---
+++ js/main.js
@@ -0,0 +1,203 @@
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
+
+const loginCheck = (user) => {
+  if (user) {
+    loggedInLinks.forEach((link) => (link.style.display = "block"));
+    loggedOutLinks.forEach((link) => (link.style.display = "none"));
+  } else {
+    loggedInLinks.forEach((link) => (link.style.display = "none"));
+    loggedOutLinks.forEach((link) => (link.style.display = "block"));
+  }
+};
+
+// SignUp
+const signUpForm = document.querySelector("#login-form");
+signUpForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const email = signUpForm["signup-email"].value;
+  const password = signUpForm["signup-pass"].value;
+  // Authenticate the User
+  auth
+    .createUserWithEmailAndPassword(email, password)
+    .then((userCredential) => {
+      // clear the form
+      signUpForm.reset();
+      // close the modal
+      $("#loginModal").modal("hide");
+    });
+});
+
+// Logout
+const logout = document.querySelector("#logout");
+
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log("signup out");
+  });
+});
+
+// SingIn
+const signInForm = document.querySelector("#login");
+
+signInForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = signInForm["login-email"].value;
+  const password = signInForm["login-password"].value;
+
+  // Authenticate the User
+  auth.signInWithEmailAndPassword(email, password).then((userCredential) => {
+    // clear the form
+    signInForm.reset();
+    // close the modal
+    $("#loginM").modal("hide");
+  });
+});
+
+// Posts
+const postList = document.querySelector(".posts");
+const setupPosts = (data) => {
+  if (data.length) {
+    let html = "";
+    data.forEach((doc) => {
+      const post = doc.data();
+      const li = `
+      <li class="list-group-item list-group-item-action">
+        <h5>${post.title}</h5>
+        <p>${post.content}</p>
+      </li>
+    `;
+      html += li;
+    });
+    postList.innerHTML = html;
+  } else {
+    postList.innerHTML = '<h4 class="text-white">Login to See Posts</h4>';
+  }
+};
+
+// events
+// list for auth state changes
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    console.log("signin");
+    fs.collection("posts")
+      .get()
+      .then((snapshot) => {
+        setupPosts(snapshot.docs);
+        loginCheck(user);
+      });
+  } else {
+    console.log("signout");
+    setupPosts([]);
+    loginCheck(user);
+  }
+});
+
+// Login with Google
+const googleButton = document.querySelector("#googleLogin");
+
+googleButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  signInForm.reset();
+  $("#signinModal").modal("hide");
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider).then((result) => {
+    console.log(result);
+    console.log("google sign in");
+  })
+  .catch(err => {
+    console.log(err);
+  })
+});
+
+// Login with Facebook
+const facebookButton = document.querySelector('#facebookLogin');
+
+facebookButton.addEventListener('click', e => {
+  e.preventDefault();
+  signInForm.reset();
+  $("#signinModal").modal("hide");
+
+  const provider = new firebase.auth.FacebookAuthProvider();
+  auth.signInWithPopup(provider).then((result) => {
+    console.log(result);
+    console.log("facebook sign in");
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
+});
+
+(function ($) {
+  "use strict";
+
+
+  /*==================================================================
+  [ Focus Contact2 ]*/
+  $('.input100').each(function(){
+      $(this).on('blur', function(){
+          if($(this).val().trim() != "") {
+              $(this).addClass('has-val');
+          }
+          else {
+              $(this).removeClass('has-val');
+          }
+      })    
+  })
+
+
+  /*==================================================================
+  [ Validate ]*/
+  var input = $('.validate-input .input100');
+
+  $('.validate-form').on('submit',function(){
+      var check = true;
+
+      for(var i=0; i<input.length; i++) {
+          if(validate(input[i]) == false){
+              showValidate(input[i]);
+              check=false;
+          }
+      }
+
+      return check;
+  });
+
+
+  $('.validate-form .input100').each(function(){
+      $(this).focus(function(){
+         hideValidate(this);
+      });
+  });
+
+  function validate (input) {
+      if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+          if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+              return false;
+          }
+      }
+      else {
+          if($(input).val().trim() == ''){
+              return false;
+          }
+      }
+  }
+
+  function showValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).addClass('alert-validate');
+  }
+
+  function hideValidate(input) {
+      var thisAlert = $(input).parent();
+
+      $(thisAlert).removeClass('alert-validate');
+  }
+  
+
+})(jQuery);
